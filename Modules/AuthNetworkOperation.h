38a731c26ef165a811006939c2b3907b0823600c
revenge <revenge@hackingteam.it>
2011-01-27 15:28:02 +0100

    Implement AUTH Network Operation

--- Modules/AuthNetworkOperation.h
+++ Modules/AuthNetworkOperation.h
@@ -1,17 +1,25 @@
-//
-//  AuthNetworkOperation.h
-//  RCSMac
-//
-//  Created by revenge on 1/13/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
+/*
+ * RCSMac - Authentication Network Operation
+ *
+ *
+ * Created by revenge on 13/01/2011
+ * Copyright (C) HT srl 2011. All rights reserved
+ *
+ */
 
 #import <Cocoa/Cocoa.h>
 #import "NetworkOperation.h"
+#import "RESTTransport.h"
 
 
 @interface AuthNetworkOperation : NSObject <NetworkOperation>
 {
+@private
+  NSData *mBackdoorSignature;
+  RESTTransport *mTransport;
 }
+
+- (id)initWithTransport: (RESTTransport *)aTransport;
+- (void)dealloc;
 
 @end
