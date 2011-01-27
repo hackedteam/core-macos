82c5276b5375192d62970b1d41b6aefccc061efb
revenge <revenge@hackingteam.it>
2011-01-27 15:18:31 +0100

    Implement SHA/Pascal categories

---
+++ Categories/NSData+Pascal.m
@@ -0,0 +1,32 @@
+//
+//  NSData+Pascal.m
+//  RCSMac
+//
+//  Created by revenge on 1/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSData+Pascal.h"
+
+
+@implementation NSData (PascalExtension)
+
+- (NSString *)unpascalizeToStringWithEncoding: (NSStringEncoding)encoding
+{
+  int len = 0;
+  [self getBytes: &len length: sizeof(int)];
+  
+  if (len > [self length])
+    {
+      return nil;
+    }
+  
+  NSData *stringData = [self subdataWithRange: NSMakeRange(4, len)];
+  
+  NSString *string = [[NSString alloc] initWithData: stringData
+                                           encoding: encoding];
+  
+  return [string autorelease];
+}
+
+@end
