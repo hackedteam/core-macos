9460d4c841aeaf7615482715802d2a42249b0d02
revenge <revenge@hackingteam.it>
2011-01-27 11:54:10 +0100

    Remove NSData related SHA1 Extension from NSString

--- Categories/NSString+SHA1.m
+++ Categories/NSString+SHA1.m
@@ -11,10 +11,8 @@
  *
  */
 
-#import <CommonCrypto/CommonDigest.h>
-#import <openssl/sha.h>
-
 #import "NSString+SHA1.h"
+#import "NSData+SHA1.h"
 
 
 @implementation NSString (SHA1)
@@ -29,36 +27,6 @@
 {
   return [[self dataUsingEncoding: NSUTF8StringEncoding
              allowLossyConversion: NO] sha1HexHash];
-}
-
-@end
-
-
-@implementation NSData (SHA1)
-
-- (NSData *)sha1Hash
-{
-  unsigned char digest[SHA_DIGEST_LENGTH];
- 	CC_SHA1([self bytes], [self length], digest);
-  
- 	return [NSData dataWithBytes: &digest length: SHA_DIGEST_LENGTH];
-}
-
-- (NSString *)sha1HexHash
-{
-  unsigned char digest[SHA_DIGEST_LENGTH];
-  char finalDigest[2 * SHA_DIGEST_LENGTH];
- 	int i;
-
- 	CC_SHA1([self bytes], [self length], digest);
-  
- 	for (i = 0; i < SHA_DIGEST_LENGTH; i++)
-    sprintf(finalDigest + i * 2, "%02x", digest[i]);
-  
-  //return [NSString stringWithCString: finalDigest
-  //                            length: 2 * SHA_DIGEST_LENGTH];
-  return [NSString stringWithCString: finalDigest
-                            encoding: NSUTF8StringEncoding];
 }
 
 @end
