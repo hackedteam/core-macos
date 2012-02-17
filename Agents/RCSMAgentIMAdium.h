6f44ad1b638510dd2baf0ccb2649d838eeb3a8bc
test1 <test1@Lionino.local>
2012-02-17 17:23:05 +0100

    Added Adium agent

---
+++ Agents/RCSMAgentIMAdium.h
@@ -0,0 +1,20 @@
+//
+//  MyClass.h
+//  RCSMac
+//
+//  Created by Alfredo Pesoli on 2/16/12.
+//  Copyright 2012 HT srl. All rights reserved.
+//
+
+#import "RCSMInputManager.h"
+
+#define ADIUM_MSG_SEND 0
+#define ADIUM_MSG_RECEIVE 1
+
+
+@interface myAIContentController : NSObject
+
+- (void) myfinishReceiveContentObject: (id)arg1;
+- (void) myfinishSendContentObject: (id)arg1;
+
+@end
