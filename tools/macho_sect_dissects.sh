1df39405e7914632f84bbc0e44787a5b61cdcd5e
armored <armored@SuperElbarto.local>
2013-06-26 14:11:27 +0200

    add __text dissections functionality

--- tools/macho_sect_dissects.sh
+++ tools/macho_sect_dissects.sh
@@ -1,22 +1,35 @@
 #!/bin/sh
 
-# macho_cstring_dissects.sh: split and delete macho cstring sects
+# macho_sect_dissects.sh: split and delete macho  sects
 # 
 # Created by Massimo Chiodini on 13/05/2013
 # Copyright (C) HT srl 2013. All rights reserved
 # 
 #  Note: output file name display the offset range of
-#  string not deleted in the __cstring section 
+#  string not deleted in the  section 
 #  from input macho file
 #
 
-FILE=$1
+OPT=$1
+FILE=$2
 
-if [ -z $FILE ]
+if [ -z $OPT ]
 then 
-	echo "  usage:  $0 <macho file i386>"
+	echo "  usage: $0 -t|-s <macho file i386>"
 	exit 0
 fi
+
+if [ $OPT == "-t" ]
+then
+	SECT="__text"
+fi
+
+if [ $OPT == "-s" ]
+then
+	SECT="__cstring"
+fi
+
+echo trying to disect section $SECT on $FILE
 
 HEX_OFFSET=`echo $FILE | sed -n 's/.*--//p' | sed -n 's/++.*//p'`
 HEX_END=`echo $FILE | sed -n 's/.*++//p'`
@@ -30,13 +43,13 @@ let "SIZE = END - OFFSET"
 
 if [ $OFFSET == "0" ]
 then
-	#echo "otool -l $FILE | grep __cstring -A 5 | sed -n 's/offset //p'"
-	TMP_OFFSET=`otool -l $FILE | grep __cstring -A 5 | sed -n 's/offset //p'`
+	#echo "otool -l $FILE | grep $SECT -A 5 | sed -n 's/offset //p'"
+	TMP_OFFSET=`otool -l $FILE | grep $SECT -A 5 | sed -n 's/offset //p'`
 
 	OFFSET=`printf "%d" $TMP_OFFSET`
 
-	#echo "otool -l $FILE | grep __cstring -A 5 | sed -n 's/size //p'"
-	HEX_SIZE=`otool -l $FILE | grep __cstring -A 5 | sed -n 's/size //p'`
+	#echo "otool -l $FILE | grep $SECT -A 5 | sed -n 's/size //p'"
+	HEX_SIZE=`otool -l $FILE | grep $SECT -A 5 | sed -n 's/size //p'`
 
 	SIZE=`printf "%d" $HEX_SIZE`
 
@@ -44,12 +57,12 @@ then
 
 	FILE_NAME=$FILE
 else
-	FILE_NAME=`echo $1 | sed -n 's/--.*//p'`
+	FILE_NAME=`echo $2| sed -n 's/--.*//p'`
 
 	let "BEGIN_A = $OFFSET"
 fi
 
-echo filename = $1 offset = $OFFSET size = $SIZE
+echo filename = $2 offset = $OFFSET size = $SIZE
 
 let "HALF_SIZE = $SIZE / 2"
 let "END_A = $BEGIN_A + $HALF_SIZE"
