c85a40c3c6b78b65d977c70568d7df0f22fd9cc1
armored <armored@SuperElbarto.local>
2014-05-09 17:07:59 +0200

    project update

--- keypress/unpacker_addr.h
+++ keypress/unpacker_addr.h
@@ -1,19 +1,21 @@
 //
 // keypress - unpacker_addr.h
 // base function pointer
-// automatically rebuilding date: Wed Apr 30 15:23:15 CEST 2014
+// automatically rebuilding date: Fri May  9 15:45:18 CEST 2014
 //
 #define _ENDCALL_ADDR (uint32_t)0x00013fb0
-#define _STRLEN_ADDR (uint32_t)0x00013020
-#define _CRPYT_PAYLOAD_ADDR (uint32_t)0x00013ac0
-#define _MH_MMAP_ADDR (uint32_t)0x000134d0
-#define _CHECK_INTEGRITY_ADDR (uint32_t)0x00013cb0
-#define _OPEN_AND_RESOLVE_ADDR (uint32_t)0x00013940
-#define _MAIN_ADDR (uint32_t)0x00012f80
-#define _ENTRYPOINT_ADDR (uint32_t)0x00013d60
-#define _BEGIN_ENC_TEXT_ADDR (uint32_t)0x00012fe0
-#define _END_ENC_TEXT_ADDR (uint32_t)0x00013d20
-#define _DMH_MMAP_ENC_V1_ADDR (uint32_t)0x000132d0
-#define _DMH_MMAP_END_V1_ADDR (uint32_t)0x00013330
-#define _DMH_MMAP_V1_ADDR (uint32_t)0x00013280
-#define _SYS_MMAP_ADDR (uint32_t)0x00013539
+#define _STRLEN_ADDR (uint32_t)0x00012c30
+#define _CRPYT_PAYLOAD_ADDR (uint32_t)0x00013910
+#define _MH_MMAP_ADDR (uint32_t)0x000131a0
+#define _CHECK_INTEGRITY_ADDR (uint32_t)0x00013b00
+#define _OPEN_AND_RESOLVE_ADDR (uint32_t)0x00013790
+#define _MAIN_ADDR (uint32_t)0x00012b90
+#define _ENTRYPOINT_ADDR (uint32_t)0x00013c10
+#define _BEGIN_ENC_TEXT_ADDR (uint32_t)0x00012bf0
+#define _END_ENC_TEXT_ADDR (uint32_t)0x00013b70
+#define _DMH_MMAP_ENC_V1_ADDR (uint32_t)0x00012ef0
+#define _DMH_MMAP_END_V1_ADDR (uint32_t)0x00012f50
+#define _DMH_MMAP_V1_ADDR (uint32_t)0x00012ea0
+#define _SYS_MMAP_ADDR (uint32_t)0x00013209
+#define _DMH_OPEN_ENC_V1_ADDR (uint32_t)0x00012f91
+#define _DMH_OPEN_END_V1_ADDR (uint32_t)0x00013000
